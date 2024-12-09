af4ccdb909548e70b99cc950dd63c15e554db751
Matias <matias.finochio@davinci.edu.ar>
2024-12-09 11:02:31 -0300

    updated code.

--- 2024/Day 1/src/Main.kt
+++ 2024/Day 1/src/Main.kt
@@ -1,19 +1,20 @@
 import java.io.File
 
 fun main() {
-    val (leftList, rightList) = File("src/list.txt").useLines{lines ->
+    val (leftList, rightList) = File("src/list.csv").useLines { lines ->
         lines.map { line ->
-            line.trim().split(Regex("\\s+")).map { it.toLong()}
-        }.toList()
-    }.let {it[0] to it[1]}
+            val (left, right) = line.split(";")
+            left.toLong() to right.toLong()
+        }.unzip()
+    }.toList()
 
     val sortedLeft = leftList.sorted()
     val sortedRight = rightList.sorted()
 
     val totalDistance = sortedLeft.zip(sortedRight)
-        .sumOf {(left, right) ->
+        .sumOf { (left, right) ->
             kotlin.math.abs(left - right)
         }
 
-    println("Total Distance: $totalDistance")
+    println("Total distance: $totalDistance")
 }
